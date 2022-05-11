5f5b8a524099cf22aa9659deb1a92584a2d5e766
soufiane kayoub <soufiane.kayoub@gmail.com>
2022-05-11 17:28:33 +0000

    first commit

---
+++ src/main/java/com/projet/ferme/FermeApplication.java
@@ -0,0 +1,13 @@
+package com.projet.ferme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FermeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/ferme/FermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.ferme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
